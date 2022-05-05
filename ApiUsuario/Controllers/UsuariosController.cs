86b73bfba261d528bbbe9baec74e6dbcab44edba
hectorsosa19 <hectorsosa931@gmail.com>
2022-05-05 11:39:27 -0400

    Api Web Service

---
+++ ApiUsuario/Controllers/UsuariosController.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ApiUsuario.Context;
+using ApiUsuario.Models;
+
+namespace ApiUsuario.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsuariosController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public UsuariosController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Usuarios
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Usuarios>>> Getusuario()
+        {
+            return await _context.usuario.ToListAsync();
+        }
+
+        // GET: api/Usuarios/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Usuarios>> GetUsuarios(int id)
+        {
+            var usuarios = await _context.usuario.FindAsync(id);
+
+            if (usuarios == null)
+            {
+                return NotFound();
+            }
+
+            return usuarios;
+        }
+
+        // PUT: api/Usuarios/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutUsuarios(int id, Usuarios usuarios)
+        {
+            if (id != usuarios.id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(usuarios).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!UsuariosExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Usuarios
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPost]
+        public async Task<ActionResult<Usuarios>> PostUsuarios(Usuarios usuarios)
+        {
+            _context.usuario.Add(usuarios);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetUsuarios", new { id = usuarios.id }, usuarios);
+        }
+
+        // DELETE: api/Usuarios/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Usuarios>> DeleteUsuarios(int id)
+        {
+            var usuarios = await _context.usuario.FindAsync(id);
+            if (usuarios == null)
+            {
+                return NotFound();
+            }
+
+            _context.usuario.Remove(usuarios);
+            await _context.SaveChangesAsync();
+
+            return usuarios;
+        }
+        [HttpGet("{username}/{password}")]
+        public ActionResult<List<Usuarios>> GetIniciarSesion(string username,string password)
+        {
+            var usuarios =  _context.usuario.Where(usuario => usuario.username.Equals(username) && usuario.password.Equals(password)).ToList();
+
+            if (usuarios == null)
+            {
+                return NotFound();
+            }
+
+            return usuarios;
+        }
+
+        private bool UsuariosExists(int id)
+        {
+            return _context.usuario.Any(e => e.id == id);
+        }
+    }
+}
---
+++ ApiUsuario/Models/Usuarios.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiUsuario.Models
+{
+    [Table("usuario")]
+    public class Usuarios
+    {
+        [Key]
+        public int id { get; set; }
+        public string nombre { get; set; }
+        public string apellido { get; set; }
+        public string username { get; set; }
+        public string password { get; set; }
+    }
+}
